3fc46e8a3ca7077d8b28a75176cada91a6f6b240
DavidAstvats <aprilade2604@gmail.com>
2021-03-11 13:31:57 +0300

    Lesson_4

---
+++ Занятие_4/1_exercise-1_variant.py
@@ -0,0 +1,23 @@
+'''
+
+1 Задание.
+Реализация алгоритма сортировки списка/массива пузырьковым методом.
+1 Вариант.
+
+'''
+
+unsorted_list = [30, 83, -16, 4, 125, 40, -13]
+
+def bubble_sort(my_list):
+    last_item = len(my_list) - 1
+    for z in range(0, last_item):
+        for x in range(0, last_item - z):
+            print(my_list)
+            if my_list[x] > my_list[x+1]:
+                my_list[x], my_list[x+1] = my_list[x+1], my_list[x]
+
+    return my_list
+
+print("Unsorted list: ", unsorted_list)
+sorted_list = bubble_sort(unsorted_list).copy()
+print("Sorted list", sorted_list)
---
+++ Занятие_4/3_exercise-2_variant.py
@@ -0,0 +1,15 @@
+'''
+
+3 задание. (свой вариант)
+Выявление элемента в списке, повторяющегося чаще всего
+
+'''
+from collections import Counter
+
+numbers = [1, 2, 3, 4, 4, 4, 5, 6, 6]
+
+def most_common(list):
+    data = Counter(list)
+    return data.most_common(1)[0][0]
+ 
+print(most_common(numbers))
